1c13c40aaea73b19aff58dbcfde07769acb18eed
Carlo Di Domenico <carlo.didomenico@hotmail.it>
2016-02-16 12:51:02 +0100

    query methods for "getById" and "getAll" implemented.

--- app/models/Input.java
+++ app/models/Input.java
@@ -4,6 +4,7 @@ import com.avaje.ebean.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.List;
 
 /**
  * Created by carlodidomenico on 09/02/16.
@@ -15,5 +16,22 @@ public class Input extends Model {
 
     public String name;
     public String type;
+
+    /**
+     * Query to DB to get a "Input" by ID
+     * @param id the id
+     * @return the Input fetched obj
+     */
+    public static Input getById(int id){
+        return (Input) new Model.Finder(Input.class).byId(id);
+    }
+
+    /**
+     * Query to get all "Input" tuples from the DB
+     * @return list with all the fetched objs
+     */
+    public static List<Input> getAll(){
+        return (List<Input>) new Model.Finder(Input.class).all();
+    }
 
 }
