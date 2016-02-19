92b0293d5297a73e02dd4912ea190890219ea0a6
Carlo Di Domenico <carlo.didomenico@hotmail.it>
2016-02-19 17:27:47 +0100

    imported cplex.jar and all the java native libraries. WARNING: this could not work with windows OS

---
+++ app/Global.java
@@ -0,0 +1,39 @@
+/**
+ * Created by carlodidomenico on 19/02/16.
+ */
+import play.*;
+import java.util.*;
+import java.lang.reflect.*;
+
+public class Global extends GlobalSettings {
+    private final String path = "./lib/";
+    @Override
+    public void onStart(Application app) {
+        PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263.jnilib").getPath());
+        PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263mpitransport.dylib").getPath());
+        PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263mpiworker.dylib").getPath());
+        PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263processtransport.dylib").getPath());
+        PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263processworker.dylib").getPath());
+        PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263remote.dylib").getPath());
+        PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263remotejni.jnilib").getPath());
+        PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263tcpiptransport.dylib").getPath());
+        PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263tcpipworker.dylib").getPath());
+
+    }
+    @Override
+    public void onStop(Application app) {
+        // NOTE: Unload library in here
+        try {
+            Field field = ClassLoader.class.getDeclaredField("nativeLibraries");
+            field.setAccessible(true);
+            Vector libs = (Vector)field.get(app.classloader());
+            for (Object lib : libs) {
+                Method finalize = lib.getClass().getDeclaredMethod("finalize", new Class[0]);
+                finalize.setAccessible(true);
+                finalize.invoke(lib, new Object[0]);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,15 @@
 package controllers;
 
+//CPLEX Libs
+import ilog.cplex.*;
+import ilog.concert.*;
+
 import models.Input;
-import models.League;
-import models.SeasonalData;
-import models.Team;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.get_started;
 import views.html.index;
+
 
 public class Application extends Controller {
 
@@ -18,9 +20,55 @@ public class Application extends Controller {
 
     public Result getStarted() {
 
+        model1();
         //return ok(get_started.render(Input.getById(1), Input.getByType("Social")));
-   	return ok(get_started.render(Input.getAll()));
+   	    return ok(get_started.render(Input.getAll()));
     }
 
+    public static void model1(){
+        try {
+            IloCplex cplex = new IloCplex();
+
+            //variables
+
+            //x,y >= 0 constraint implicit
+            IloNumVar 	x = cplex.numVar(0, Double.MAX_VALUE, "x"),
+                    y = cplex.numVar(0, Double.MAX_VALUE, "y");
+
+            //expressions
+            IloLinearNumExpr objective = cplex.linearNumExpr();
+
+            objective.addTerm(0.12, x);
+            //.addTerm(1, x); if you want x multiplied by 1
+            objective.addTerm(0.15, y);
+
+            //define objective
+
+            cplex.addMinimize(objective);
+
+            //define constraints
+            //60x + 60y >= 300
+            cplex.addGe(cplex.sum(cplex.prod(60, x),cplex.prod(60,y)), 300);
+            //12x + 6y  >= 36
+            cplex.addGe(cplex.sum(cplex.prod(12, x),cplex.prod(6,y)), 36);
+            //10x + 30y >= 90
+            cplex.addGe(cplex.sum(cplex.prod(10, x),cplex.prod(30,y)), 90);
+
+
+            //solve
+            if(cplex.solve()){
+                System.out.println("obj = " + cplex.getObjValue());
+                System.out.println("x = " + cplex.getValue(x));
+                System.out.println("y = " + cplex.getValue(y));
+
+
+            }else{
+                System.out.println("Model Not solved");
+            }
+
+        } catch (IloException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
