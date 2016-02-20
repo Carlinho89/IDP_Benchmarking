35d67497729796cc463f7fb828d93379804b0b5c
Carlo Di Domenico <carlo.didomenico@hotmail.it>
2016-02-20 18:05:34 +0100

    libs reordering

--- app/Global.java
+++ app/Global.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.lang.reflect.*;
 
 public class Global extends GlobalSettings {
-    private final String path = "./lib/";
+    private String path = "./lib/";
     @Override
     public void onStart(Application app) {
 
@@ -14,27 +14,30 @@ public class Global extends GlobalSettings {
         System.out.println("OS is = " + os);
         if(os.contains("mac os x") || os.contains("mac")) {
             // OSX/Linux:
-            PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263.jnilib").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263mpitransport.dylib").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263mpiworker.dylib").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263processtransport.dylib").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263processworker.dylib").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263remote.dylib").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263remotejni.jnilib").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263tcpiptransport.dylib").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/libcplex1263tcpipworker.dylib").getPath());
+            path += "OSX_Linux";
+            PlayNativeLibLoader.load(app.getFile(path +"libcplex1263.jnilib").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"libcplex1263mpitransport.dylib").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"libcplex1263mpiworker.dylib").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"libcplex1263processtransport.dylib").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"libcplex1263processworker.dylib").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"libcplex1263remote.dylib").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"libcplex1263remotejni.jnilib").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"libcplex1263tcpiptransport.dylib").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"libcplex1263tcpipworker.dylib").getPath());
 
         }else if (os.contains("windows")){
 	    //WINDOWS:
-	        PlayNativeLibLoader.load(app.getFile("./lib/cplex1260.dll").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/cplex1260processtransport.dll").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/cplex1260processworker.dll").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/cplex1260remote.dll").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/cplex1260remotejni.dll").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/cplex1260tcpiptransport.dll").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/cplex1260tcpipworker.dll").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/ILOG.Concert.dll").getPath());
-            PlayNativeLibLoader.load(app.getFile("./lib/ILOG.CPLEX.dll").getPath());
+            path += "WIN";
+
+            PlayNativeLibLoader.load(app.getFile(path +"cplex1260.dll").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"cplex1260processtransport.dll").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"cplex1260processworker.dll").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"cplex1260remote.dll").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"cplex1260remotejni.dll").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"cplex1260tcpiptransport.dll").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"cplex1260tcpipworker.dll").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"ILOG.Concert.dll").getPath());
+            PlayNativeLibLoader.load(app.getFile(path +"ILOG.CPLEX.dll").getPath());
 
         }else {
             System.out.println("Something went wrong loadinc CPLEX libs");
