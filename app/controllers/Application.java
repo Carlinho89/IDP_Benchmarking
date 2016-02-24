543c5835ffdce6576c93d753404d513596c310d5
Carlo Di Domenico <carlo.didomenico@hotmail.it>
2016-02-24 11:57:07 +0100

    merge culex and get started features

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,10 @@
 package controllers;
 
 //CPLEX Libs
-import ilog.cplex.*;
-import ilog.concert.*;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import models.Input;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.get_started;
@@ -12,6 +12,7 @@ import views.html.index;
 
 
 public class Application extends Controller {
+
 
     public Result index() {
 
@@ -22,10 +23,29 @@ public class Application extends Controller {
 
         CplexController cplexController = new CplexController();
         cplexController.model1();
+
         //return ok(get_started.render(Input.getById(1), Input.getByType("Social")));
-   	    return ok(get_started.render(Input.getAll()));
+        return ok(get_started.render(Input.getByType("Sporty"),Input.getByType("Social"),Input.getByType("Monetary")));
+
     }
 
 
+    @BodyParser.Of(BodyParser.Json.class)
+    public  Result sayHello() {
+        JsonNode json = request().body().asJson();
+        String name = json.findPath("leagueID").textValue();
+        if(name == null) {
+            return badRequest("Missing parameter [name]"+request().body().asJson());
+        } else {
+            return ok(json);
 
+        }
+    }
+
+
+    public  Result showJSON() {
+        JsonNode json = request().body().asJson();
+        String a = json.path("leagueID").asText();
+        return ok(index.render(a));
+    }
 }
